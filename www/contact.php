5f0b1361683ec6a4b4c91a03864b946f84c51243
rogerio <rogerio@mail.com>
2017-06-24 13:17:40 -0300

    mudança de layout, envio de email

--- www/contact.php
+++ www/contact.php
@@ -1,33 +1,7 @@
 <?php
 
-if(isset($_POST['email'])) {
 
-    $email_to = "YOUR EMAIL ID GOES HERE";
- 
-    $email_subject = "Website Contact";
-
- 
-    function died($error) {
- 
-        // your error code can go here
- 
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
- 
-        echo "These errors appear below.<br /><br />";
- 
-        echo $error."<br /><br />";
- 
-        echo "Please go back and fix these errors.<br /><br />";
- 
-        die();
- 
-    }
- 
-     
- 
-    // validation expected data exists
- 
-    if(!isset($_POST['name']) ||
+ if(!isset($_POST['name']) ||
  
         !isset($_POST['email']) ||
  
@@ -41,54 +15,41 @@ if(isset($_POST['email'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
- 
- 
-    $email_message = "Form details below.\n\n";
- 
-     
- 
-    function clean_string($string) {
- 
-      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-      return str_replace($bad,"",$string);
- 
-    }
- 
-     
- 
-    $email_message .= "Name: ".clean_string($name)."\n";
- 
-    $email_message .= "Email: ".clean_string($email)."\n";
- 
-    $email_message .= "Message: ".clean_string($message)."\n";
 
- 
-	// create email headers
-	 
-	$headers = 'From: '.$email."\r\n".
-	 
-	'Reply-To: '.$email."\r\n" .
-	 
-	'X-Mailer: PHP/' . phpversion();
-	 
-	@mail($email_to, $email_subject, $email_message, $headers);  
+// Passando os dados obtidos pelo formuláo para as variáis abaixo
+$quebra_linha = "\n";
+$emailsender= 'contato@mcc.empresarial.ws';
+$nomeremetente     = $name;
+$emailremetente    =  $email;
+$emaildestinatario = "advogadosccm@gmail.com";
+$comcopia          = "";
+$comcopiaoculta    = "";
+$assunto           = $name;
+$mensagem          =  $message;
 
 
+/* Montando a mensagem a ser enviada no corpo do e-mail. */
+$mensagemHTML = 'Rementente: '.$email.'
+<p>'.$mensagem.'</p>
+<hr>';
+
+
+/* Montando o cabeçho da mensagem */
+$headers = "MIME-Version: 1.1".$quebra_linha;
+$headers .= "Content-type: text/html; charset=iso-8859-1".$quebra_linha;
+// Perceba que a linha acima conté"text/html", sem essa linha, a mensagem nãchegaráormatada.
+$headers .= "From: ".$emailsender.$quebra_linha;
+$headers .= "Return-Path: " . $emailsender . $quebra_linha;
+$headers .= "Cc: ".$comcopia.$quebra_linha;
+$headers .= "Bcc: ".$comcopiaoculta.$quebra_linha;
+$headers .= "Reply-To: ".$emailremetente.$quebra_linha;
+// Note que o e-mail do remetente serásado no campo Reply-To (Responder Para)
+
+/* Enviando a mensagem */
+mail($emaildestinatario, $assunto, $mensagemHTML, $headers, "-r". $emailsender);
+
+/* Mostrando na tela as informaçs enviadas por e-mail *
+print "Mensagem enviada com sucesso!"*/
 ?>
- 
- 
- 
-<!-- include your own success html here -->
- 
- 
- 
-Thank you for contacting me. Will be in touch with you very soon.
- 
- 
- 
-<?php
- 
-}
- 
-?>
+
+
